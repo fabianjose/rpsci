d59bee961e79841e0c38ebf0aee2d2c59520e45c
web 02 <moncki21@gmail.com>
2020-01-24 17:48:35 -0400

    Small fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,10 +8,10 @@
 
           <div class="col-md-4 col-sm-6 col-xs-12">
             <div class="info-box">
-              <span class="info-box-icon bg-info"><i class="fas fa-suitcase"></i></span>
+              <span class="info-box-icon bg-primary"><i class="fas fa-suitcase"></i></span>
               <div class="info-box-content">
                 <h5 class="info-box-text">Empresas</h5>
-                <a href="/companies" class="btn btn-block btn-outline-info">
+                <a href="/companies" class="btn btn-block btn-outline-primary">
                   Gestión  <i class="fas fa-arrow-circle-right"></i>
                 </a>
               </div>
@@ -36,6 +36,17 @@
               <div class="info-box-content">
                 <h5 class="info-box-text">Ofertas</h5>
                 <a href="/offers" class="btn btn-block btn-outline-danger">
+                  Gestión  <i class="fas fa-arrow-circle-right"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-6 col-xs-12">
+            <div class="info-box">
+              <span class="info-box-icon bg-info"><i class="fas fa-box"></i></span>
+              <div class="info-box-content">
+                <h5 class="info-box-text">Banners Publicitarios</h5>
+                <a href="/banners" class="btn btn-block btn-outline-info">
                   Gestión  <i class="fas fa-arrow-circle-right"></i>
                 </a>
               </div>
