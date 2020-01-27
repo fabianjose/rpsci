9a7126ff4279d418a42e22f3aa82ef83508eeab5
web 02 <moncki21@gmail.com>
2020-01-27 15:47:05 -0400

    usuario

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -24,7 +24,7 @@
                 <form action="/login" method="post">
                     {{ csrf_field() }}
                     <div class="input-group mb-3">
-                        <input type="text" name="username" class="form-control" value="" placeholder="Usuario" autofocus>
+                        <input type="text" name="username" class="form-control" value="" placeholder="Nombre de usuario" autofocus>
                         <div class="input-group-append">
                             <div class="input-group-text">
                                 <span class="fas fa-user"></span>
