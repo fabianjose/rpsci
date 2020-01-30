e5cad861e7e799beced1ba0796f60fd5edcd35c5
web 02 <moncki21@gmail.com>
2020-01-30 14:12:19 -0400

    .

--- public/blog/wp-config.php
+++ public/blog/wp-config.php
@@ -17,8 +17,8 @@
  *
  * @package WordPress
  */
- // define( 'WP_HOME', 'https://intercolombia.nextscale.net/blog' );
- // define( 'WP_SITEURL', 'https://intercolombia.nextscale.net/blog' );
+ define( 'WP_HOME', 'https://intercolombia.nextscale.net/blog' );
+ define( 'WP_SITEURL', 'https://intercolombia.nextscale.net/blog' );
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
 define( 'DB_NAME', 'wp_intercolombia' );
