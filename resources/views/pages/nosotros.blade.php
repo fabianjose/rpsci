e76ebd48240aed15fd9216873c289bfb26d86872
fabian <fabian@remotepcsolutions.com>
2020-11-23 16:47:21 -0500

    modificando iconos en nosotros

--- resources/views/pages/nosotros.blade.php
+++ resources/views/pages/nosotros.blade.php
@@ -9,7 +9,7 @@
 		<h2 class="font-weight-bold text-white nosotros-title-text">Nosotros</h2>
 	</div>
 	
-<br><br><br>
+			<br><br><br>
 			<div class="container">
 
 
@@ -118,5 +118,47 @@
 				</div>
 				<br><br><br>
 			</div>
+
+
+			<div class="d-flex flex-column nosotros-services-bg p-4 align-items-center">
+		<h4 class="nosotros-services-title my-2">SERVICIOS OFRECIDOS</h4>
+		<div class="d-flex flex-wrap justify-content-center mt-4 p-4">
+			<div class="col-xl-4 col-lg-4 col-md-6 col-12 d-flex align-items-center justify-content-center p-2">
+				<img src="{{url('images/service-1.png')}}" class="img-nosotros-services">
+				<div class="pl-2 d-flex flex-column">
+					<span class="text-uppercase font-weight-bold text-black">MPLS</span>
+					<button type="button" class="btn rounded-pill text-uppercase nosotros-service-text text-sm nosotros-service-btn">Ver Ofertas</button>
+				</div>
+			</div>
+			<div class="col-xl-4 col-lg-4 col-md-6 col-12 d-flex align-items-center justify-content-center p-2">
+				<img src="{{url('images/service-2.png')}}" class="img-nosotros-services">
+				<div class="pl-2 d-flex flex-column">
+					<span class="text-uppercase font-weight-bold text-black">Internet satelital</span>
+					<button type="button" class="btn rounded-pill text-uppercase nosotros-service-text text-sm nosotros-service-btn">Ver Ofertas</button>
+				</div>
+			</div>
+			<div class="col-xl-4 col-lg-4 col-md-6 col-12 d-flex align-items-center justify-content-center p-2">
+				<img src="{{url('images/service-3.png')}}" class="img-nosotros-services">
+				<div class="pl-2 d-flex flex-column">
+					<span class="text-uppercase font-weight-bold text-black">Telefonía IP</span>
+					<button type="button" class="btn rounded-pill text-uppercase nosotros-service-text text-sm nosotros-service-btn">Ver Ofertas</button>
+				</div>
+			</div>
+			<div class="col-xl-4 col-lg-4 col-md-6 col-12 d-flex align-items-center justify-content-center p-2">
+				<img src="{{url('images/service-4.png')}}" class="img-nosotros-services">
+				<div class="pl-2 d-flex flex-column">
+					<span class="text-uppercase font-weight-bold text-black">Internet</span>
+					<button type="button" class="btn rounded-pill text-uppercase nosotros-service-text text-sm nosotros-service-btn">Ver Ofertas</button>
+				</div>
+			</div>
+			<div class="col-xl-4 col-lg-4 col-md-6 col-12 d-flex align-items-center justify-content-center p-2">
+				<img src="{{url('images/service-5.png')}}" class="img-nosotros-services">
+				<div class="pl-2 d-flex flex-column">
+					<span class="text-uppercase font-weight-bold text-black">Televisión</span>
+					<button type="button" class="btn rounded-pill text-uppercase nosotros-service-text text-sm nosotros-service-btn">Ver Ofertas</button>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
 @stop
