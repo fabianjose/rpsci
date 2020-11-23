680d58655764cbb72cec5d4d9dc77974af6f6448
David José Márquez Batiz <djmbdv@gmail.com>
2020-11-23 11:52:30 -0400

    merge

--- resources/js/components/items/offers/offercard.vue
+++ resources/js/components/items/offers/offercard.vue
@@ -1,83 +1,30 @@
 <template>
   <div class="offer-card d-flex flex-column">
     <div class="offer-card-header">
-      <img :src="baseUrl+'/storage/'+offer.company_logo" alt="logo" class="h-100 ">
+      <img :src="baseUrl+'/storage/'+offer.company_logo" alt="logo" class="h-100 img-fluid">
     </div>
     <div :class="'offer-card-separator '+(index%2?'bg-main-blue':'bg-main-pink')"></div>
-
     <div class="offer-card-content">
-      <div class="row" style="    padding-left: 30px;  padding-right:30px; margin-top: -12px;">
-       <!-- <h6 class="col-12 offer-card-title">{{offer.company_name}}</h6> -->
-       <h6 class="col-12 offer-card-title">
-         
-         {{offer.fields_values[0].value}} 
-         
-         </h6>       
+      <div class="row">
+        <h6 class="col-12 offer-card-title">{{offer.company_name}}</h6>
       </div>
-      <div class="row text-center">
-       <!--  <h6 class="col-12 offer-card-benefits text-wrap" style="font-family:heebo">{{offer.benefits}}</h6>-->
-        <p class="col-12" style="font-family: 'Heebo';    margin-top: -9px; color:#88868b">velocidad</p>
-        <!-- <h6 class="col-12 offer-card-title">{{offer.company_name}}</h6>  ACA VA EL VALOR DE LA VELOCIDAD -->
-        <h1 class="col-12 " style="font-family: 'Heebo';    color: #5b5b5f;    margin-top: -18px; font-weight: 800;"> 
-   
-               
-              {{offer.fields_values[1].value}} 
-            
-      
-  </h1>
-         
-
-          
-
-
-
-       
+      <div class="row">
+        <h6 class="col-12 offer-card-benefits text-wrap" style="font-family:Montserrat-regular">{{offer.benefits}}</h6>
       </div> 
-          <div :class="'offer-card-separator '+(index%2?'bg-main-blue':'bg-main-pink')"></div>
-
-            <div class="row text-center">
-       <!--  <h6 class="col-12 offer-card-benefits text-wrap" style="font-family:heebo">{{offer.benefits}}</h6>-->
-        <p class="col-12 " style="font-family: 'Heebo';    color:#88868b">Cantidad de Canales</p>
-        <!-- <h6 class="col-12 offer-card-title">{{offer.company_name}}</h6>  ACA VA EL VALOR DE CANALES DE BASE DEDATOS -->
-        <h1 class="col-12 " style="font-family: 'Heebo';    color: #5b5b5f;     margin-top: -19px;  font-weight: 800;   "> {{offer.canales}}</h1>
-      </div> 
-
-         <div :class="'offer-card-separator '+(index%2?'bg-main-blue':'bg-main-pink')"></div>
-
-            <div class="row text-center">
-       <!--  <h6 class="col-12 offer-card-benefits text-wrap" style="font-family:heebo">{{offer.benefits}}</h6>-->
-        <p class="col-12 " style="font-family: 'Heebo';    color:#88868b">Telefonia</p>
-        <!-- <h6 class="col-12 offer-card-title">{{offer.company_name}}</h6>  ACA VA EL VALOR DE CANALES DE BASE DEDATOS -->
-        <h1 class="col-12 " style="font-family: 'Heebo';    color: #5b5b5f;     margin-top: -19px; font-weight: 800;  ">ILIMITADA</h1>
-      </div> 
-        <div class="row"  style="    padding-left: 30px;  padding-right:30px; margin-top: ;">
-             <h6 class="col-12 offer-card-title">descuento del 50% 4 meses</h6>  
-        </div>
-                  
-    <p class="col-12 " style="font-family: 'Heebo';    color:#88868b">Desde</p>
-
-      <div class="row" >
-        <h5 class="col-12 offer-card-price" >$ {{showPrice(offer.tariff)}}</h5>
+      <div class="row">
+        <h5 class="col-12 offer-card-price">$ {{showPrice(offer.tariff)}}</h5>
       </div>
     </div>
     <div class="offer-card-footer">
       <div class="col-10">
-        <div>
-  <button @click="emitContact" data-toggle="modal" data-target="#modalConsultOffer" 
-          :class="'btn  text-white offer-card-btn rounded-pill '+(index%2?'bg-main-blue':'bg-main-pink')">
+        <button @click="emitContact" data-toggle="modal" data-target="#modalConsultOffer"
+          :class="'btn btn-block text-white offer-card-btn rounded-pill '+(index%2?'bg-main-blue':'bg-main-pink')">
           ADQUIÉRELO
         </button>
-        </div>
-      
       </div>
     </div>
   </div>
-
-
-
-  
 </template>
-
 
 <script>
 export default {
