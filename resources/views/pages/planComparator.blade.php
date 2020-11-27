2f54dd6ea0077ecff48d51ff084478df0a5d44ae
David José Márquez Batiz <djmbdv@gmail.com>
2020-11-27 15:00:45 -0400

    searchfilter
    filtro
    
    Co-Authored-By: fabianjose <28871429+fabianjose@users.noreply.github.com>

--- resources/views/pages/planComparator.blade.php
+++ resources/views/pages/planComparator.blade.php
@@ -3,7 +3,9 @@
 @section("content-page")
 
     <offers-filter :pagination="{{$pagination->toJson()}}" :fields="{{$fields}}" 
-        query="{{$query}}" :lastpage="{{$last_page}}" :providers="{{$providers}}" >
+        query="{{$query}}" :lastpage="{{$last_page}}" :providers="{{$providers}}"
+        :technologies = "{{$technologies}}"
+        :speeds="{{$speeds}}" >
     </offers-filter>
 
 @stop
